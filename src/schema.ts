8a3e7aef8ad113f944aba55862d7b73b893ec277
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-07-23 19:01:59 +0800

    add docs to instance

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,24 @@
 export default interface Schema {
+    /**
+     * Unique identifier for the record
+     */
     id: string;
+    /**
+     * Timestamp when the record was created
+     */
     createdAt: Date;
+    /**
+     * Timestamp when the record was last updated
+     */
     updatedAt: Date;
 
+    /**
+     * Save this instance
+     * @returns the saved instance
+     */
     save(): Schema;
+    /**
+     * Delete this instance
+     */
     delete();
 }
