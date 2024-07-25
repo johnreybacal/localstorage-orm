d1778776660560106cbd552ce56aefa731d06afe
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-07-25 14:04:10 +0800

    fix test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -10,136 +10,152 @@ const createPersonModel = (modelSettings?: ModelSettings) => {
     const personModel = new Model<Person>("person", modelSettings);
     return personModel;
 };
+const modelSettings: ModelSettings[] = [
+    {},
+    {
+        timestamps: true,
+        softDelete: true,
+    },
+];
 
 describe.each([
-    [
-        {},
-        {
-            timestamps: true,
-            softDelete: true,
-        },
-    ],
-])(`Basic functions: Creating`, (modelSettings: ModelSettings) => {
-    test("Instantiate a model", () => {
-        const personModel = createPersonModel(modelSettings);
+    {},
+    {
+        timestamps: true,
+        softDelete: true,
+    },
+])(
+    "Basic functions: Creating [$modelSettings]",
+    (modelSettings: ModelSettings) => {
+        test("Instantiate a model", () => {
+            const personModel = createPersonModel(modelSettings);
 
-        expect(personModel).toBeDefined();
-        expect(personModel).toBeInstanceOf(Model);
-    });
-    test("Build an instance", () => {
-        const personModel = createPersonModel(modelSettings);
+            expect(personModel).toBeDefined();
+            expect(personModel).toBeInstanceOf(Model);
+        });
+        test("Build an instance", () => {
+            const personModel = createPersonModel(modelSettings);
 
-        const person = personModel.build();
+            const person = personModel.build();
 
-        expect(person).toBeDefined();
-    });
-    test("Build and save an instance", () => {
-        const personModel = createPersonModel(modelSettings);
+            expect(person).toBeDefined();
+        });
+        test("Build and save an instance", () => {
+            const personModel = createPersonModel(modelSettings);
 
-        const person = personModel.build();
+            const person = personModel.build();
 
-        person.name = "John Doe";
-        person.age = 24;
+            person.name = "John Doe";
+            person.age = 24;
 
-        const result = person.save();
+            const result = person.save();
 
-        expect(person.id).toBeDefined();
-        expect(result.id).toBeDefined();
-        expect(result).toEqual(person);
-
-        if (modelSettings.timestamps) {
-            expect(result.createdAt).toBeInstanceOf(Date);
-        }
-    });
-    test("Create an instance", () => {
-        const personModel = createPersonModel(modelSettings);
-
-        const data = {
-            name: "Jane Doe",
-            age: 24,
-        };
-
-        const person = personModel.create(data);
-
-        expect(person).toBeDefined();
-
-        expect(person.id).toBeDefined();
-        expect(person).toEqual(expect.objectContaining(data));
-        if (modelSettings.timestamps) {
-            expect(person.createdAt).toBeInstanceOf(Date);
-        }
-    });
-    test("Bulk build an array of instance", () => {
-        const personModel = createPersonModel(modelSettings);
-
-        const dataArr = [
-            {
-                name: "Bulk Person 1",
-                age: 1,
-            },
-            {
-                name: "Bulk Person 2",
-                age: 2,
-            },
-        ];
-
-        const persons = personModel.build(dataArr);
-
-        expect(persons.length).toEqual(2);
-    });
-    test("Bulk build and save an array of instance", () => {
-        const personModel = createPersonModel(modelSettings);
-
-        const dataArr = [
-            {
-                name: "Bulk Person 1-1",
-                age: 1,
-            },
-            {
-                name: "Bulk Person 2-1",
-                age: 2,
-            },
-        ];
-
-        const persons = personModel.build(dataArr);
-
-        persons.save();
-
-        persons.forEach((person) => {
             expect(person.id).toBeDefined();
+            expect(result.id).toBeDefined();
+            expect(result).toEqual(person);
+
+            if (modelSettings.timestamps) {
+                expect(result.createdAt).toBeInstanceOf(Date);
+            }
+        });
+        test("Create an instance", () => {
+            const personModel = createPersonModel(modelSettings);
+
+            const data = {
+                name: "Jane Doe",
+                age: 24,
+            };
+
+            const person = personModel.create(data);
+
+            expect(person).toBeDefined();
+
+            expect(person.id).toBeDefined();
+            expect(person).toEqual(expect.objectContaining(data));
             if (modelSettings.timestamps) {
                 expect(person.createdAt).toBeInstanceOf(Date);
             }
         });
-    });
-    test("Bulk create an array of instance", () => {
-        const personModel = createPersonModel(modelSettings);
+        test("Bulk build an array of instance", () => {
+            const personModel = createPersonModel(modelSettings);
 
-        const dataArr = [
-            {
-                name: "Bulk Person 1-2",
-                age: 1,
-            },
-            {
-                name: "Bulk Person 2-2",
-                age: 2,
-            },
-        ];
+            const dataArr = [
+                {
+                    name: "Bulk Person 1",
+                    age: 1,
+                },
+                {
+                    name: "Bulk Person 2",
+                    age: 2,
+                },
+            ];
 
-        const persons = personModel.create(dataArr);
+            const persons = personModel.build(dataArr);
 
-        persons.forEach((person) => {
-            expect(person.id).toBeDefined();
-            if (modelSettings.timestamps) {
-                expect(person.createdAt).toBeInstanceOf(Date);
-            }
+            expect(persons.length).toEqual(2);
         });
-    });
-});
+        test("Bulk build and save an array of instance", () => {
+            const personModel = createPersonModel(modelSettings);
 
-// describe("Basic functions: Fetching", () => {
+            const dataArr = [
+                {
+                    name: "Bulk Person 1-1",
+                    age: 1,
+                },
+                {
+                    name: "Bulk Person 2-1",
+                    age: 2,
+                },
+            ];
+
+            const persons = personModel.build(dataArr);
+
+            persons.save();
+
+            persons.forEach((person) => {
+                expect(person.id).toBeDefined();
+                if (modelSettings.timestamps) {
+                    expect(person.createdAt).toBeInstanceOf(Date);
+                }
+            });
+        });
+        test("Bulk create an array of instance", () => {
+            const personModel = createPersonModel(modelSettings);
+
+            const dataArr = [
+                {
+                    name: "Bulk Person 1-2",
+                    age: 1,
+                },
+                {
+                    name: "Bulk Person 2-2",
+                    age: 2,
+                },
+            ];
+
+            const persons = personModel.create(dataArr);
+
+            persons.forEach((person) => {
+                expect(person.id).toBeDefined();
+                if (modelSettings.timestamps) {
+                    expect(person.createdAt).toBeInstanceOf(Date);
+                }
+            });
+        });
+    }
+);
+
+// describe.each([
+//     [
+//         {},
+//         {
+//             timestamps: true,
+//             softDelete: true,
+//         },
+//     ],
+// ])(`Basic functions: Fetching`, (modelSettings: ModelSettings) => {
 //     test("List", () => {
-//         const personModel = createPersonModel();
+//         const personModel = createPersonModel(modelSettings);
 
 //         // personModel.
 //     });
