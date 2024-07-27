277ea6ea798570bd161041165232e9de36f67a69
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-07-27 14:26:49 +0800

    refactor: hide functions

--- src/instanceMethods.ts
+++ src/instanceMethods.ts
@@ -1,17 +1,14 @@
-import LocalStorageDb from "./localStorageDb";
+import { localStorageDb } from "./localStorageDb";
 import ModelManager from "./modelManager";
 import ModelSettings from "./modelSettings";
 import Schema from "./schema";
 
 export default class InstanceMethods<T extends Schema> {
-    private localStorageDb: LocalStorageDb<T>;
     private modelSettings: ModelSettings;
+    private modelName: string;
 
-    constructor(
-        localStorageCrud: LocalStorageDb<T>,
-        modelSettings: ModelSettings
-    ) {
-        this.localStorageDb = localStorageCrud;
+    constructor(modelName: string, modelSettings: ModelSettings) {
+        this.modelName = modelName;
         this.modelSettings = modelSettings;
     }
 
@@ -45,32 +42,36 @@ export default class InstanceMethods<T extends Schema> {
         return instanceOfSchema;
     }
 
-    public save(record: T) {
+    private save(record: T) {
         if (record.id) {
             this.setUpdateTimestamp(record);
 
-            const updatedRecord = this.localStorageDb.update(record.id, record);
+            const updatedRecord = localStorageDb.update(
+                this.modelName,
+                record.id,
+                record
+            );
 
             return this.build(updatedRecord);
         } else {
             this.setCreateTimestamp(record);
             record.id = crypto.randomUUID();
 
-            const createdRecord = this.localStorageDb.create(record);
+            const createdRecord = localStorageDb.create(this.modelName, record);
 
             return this.build(createdRecord);
         }
     }
 
-    public delete(record: T) {
+    private delete(record: T) {
         if (this.modelSettings.softDelete) {
-            this.localStorageDb.softDelete(record.id);
+            localStorageDb.softDelete(this.modelName, record.id);
         } else {
-            this.localStorageDb.delete(record.id);
+            localStorageDb.delete(this.modelName, record.id);
         }
     }
 
-    public populate(record: T, path: string, index?: number) {
+    private populate(record: T, path: string, index?: number) {
         let refs = this.modelSettings.references.filter(
             ({ property }) => property === path
         );
